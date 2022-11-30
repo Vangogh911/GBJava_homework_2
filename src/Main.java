5fbfad18db929ca198e0f7c3d2ac68379290cb27
master <frami@framin1991.com>
2022-11-30 22:48:50 +0500

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,3 @@
-// def bose_nelson(data):
-//     m = 1
-//     while m < len(data):
-//         j = 0
-//         while j + m < len(data):
-//             bose_nelson_merge(j, m, m)
-//             j = j + m + m
-//         m = m + m
-//     return data
-//
-// def bose_nelson_merge(j, r, m):
-//     if j + r < len(data):
-//         if m == 1:
-//             if data[j] > data[j + r]:
-//                 data[j], data[j + r] = data[j + r], data[j]
-//         else:
-//             m = m // 2
-//             bose_nelson_merge(j, r, m)
-//             if j + r + m < len(data):
-//                 bose_nelson_merge(j + m, r, m)
-//             bose_nelson_merge(j + m, r - m, m)
-//     return data
-
 import java.util.Arrays;
 import java.util.Random;
 
